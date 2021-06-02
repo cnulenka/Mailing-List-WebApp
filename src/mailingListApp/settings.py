5c4607f4edba97f924092f0340531a3fc41ac87b
cnulenka <sinulenka001@gmail.com>
2021-06-03 03:57:23 +0530

    added core app, subscriber model

--- src/mailingListApp/settings.py
+++ src/mailingListApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     'authApp',
     'rest_framework',
     'rest_framework.authtoken',
-    'djoser'
+    'djoser',
+    'core',
 ]
 
 REST_FRAMEWORK = {
@@ -149,3 +151,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FROM_EMAIL =  os.environ.get('FROM_EMAIL')
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
